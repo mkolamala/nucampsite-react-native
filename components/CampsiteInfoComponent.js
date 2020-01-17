caa71852ad94ed540f42a7463f677763c3a01cbc
Krishna Kolamala <krishnakolamala318@gmail.com>
2020-01-16 16:44:31 -0800

    Gestures Part 2

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -33,10 +33,18 @@ const mapDispatchToProps = {
 
 function RenderCampsite(props) {
   const campsite = props.campsite;
+  handleViewRef = ref => (this.view = ref);
   const recognizeDrag = ({ dx }) => (dx < -200 ? true : false);
 
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      this.view
+        .rubberBand(1000)
+        .then(endState =>
+          console.log(endState.finished ? "finished" : "canceled")
+        );
+    },
     onPanResponderEnd: (e, gestureState) => {
       console.log("pan responder end", gestureState);
       if (recognizeDrag(gestureState)) {
@@ -66,7 +74,13 @@ function RenderCampsite(props) {
 
   if (campsite) {
     return (
-      <Animatable.View animation="fadeInDown" duration={2000} delay={1000} {...panResponder.panHandlers}>
+      <Animatable.View
+        animation="fadeInDown"
+        duration={2000}
+        delay={1000}
+        ref={this.handleViewRef}
+        {...panResponder.panHandlers}
+      >
         <Card
           featuredTitle={campsite.name}
           image={{ uri: baseUrl + campsite.image }}
