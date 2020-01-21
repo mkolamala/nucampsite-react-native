bba3f7df577421fd20944afa523db765576667e7
Krishna Kolamala <krishnakolamala318@gmail.com>
2020-01-20 23:06:20 -0800

    Secure Store

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://10.167.71.135:3001/';
-//export const baseUrl = 'http://192.168.0.17:3001/';
+//export const baseUrl = 'http://10.167.71.135:3001/';
+export const baseUrl = 'http://192.168.0.17:3001/';
